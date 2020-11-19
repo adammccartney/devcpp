ff99ce7843af179f41ca2530c8d4e96f4f861815
adammccartney <adam@mur.at>
2020-11-19 18:13:12 +0100

    added execpt Makefile clause to gitignore

---
+++ day01/guess_number/guess_number.cpp
@@ -0,0 +1,11 @@
+/*
+Zahlenratespiel gegen den Computer
+- Der Computer bestimmt eine Zufallszahl zwischen 1 und 100
+- Der Anwender soll diese Zahl erraten und hat maximal 9 Versuche, schafft er es nicht, 
+  die Zahl zu erraten, hat er verloren
+- Pro Rateversuch passiert folgendes:
+-- Der Anwender gibt eine Zahl ein, diese Zahl wird vom Computer verglichen
+-- Zahl > Zufallszahl => Ausgabe "Zahl zu gross"
+-- Zahl < Zufallszahl => Ausgabe "Zahl zu kleine"
+-- Zahl == Zufallszahl => Ausgabe "Spiel gewonnen"
+*/
---
+++ day01/number_to_text/number_to_text.cpp
@@ -0,0 +1,8 @@
+/*
+Der Anwender gibt eine Ganzzahl ein.
+Das Programm ermittelt die einzelnen Ziffern und setzt sie in Worte um.
+Die Worte sollen als ein Textstring zur Verfügung gestellt werden. Beispiel:
+
+Eingabe: 326
+Ausgabe: drei zwei sechs
+*/
